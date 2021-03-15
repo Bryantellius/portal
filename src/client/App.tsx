ebdc6e290e3a761250e3755297cd71ba3e3a93fa
Ben <bbryant@truecoders.io>
2021-03-15 16:36:37 -0500

    Added logic for dark mode

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -6,6 +6,36 @@ import GitIntro from "./views/GitIntro";
 import Home from "./views/Home";
 
 const App: React.FC = () => {
+  const [isLoaded, setIsLoaded] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isLoaded) {
+      darkModeLoader();
+    }
+    setIsLoaded(true);
+  }, [isLoaded]);
+
+  const darkModeLoader = () => {
+    const toggleMode = document.getElementById("checkbox") as HTMLInputElement;
+    const currentMode = localStorage.getItem("theme");
+    if (currentMode) {
+      document.documentElement.setAttribute("data-theme", currentMode);
+      if (currentMode === "dark") {
+        toggleMode.checked = true;
+      }
+    }
+    function changeTheme(event: any) {
+      if (event.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+      }
+    }
+    toggleMode.addEventListener("change", changeTheme);
+  };
+
   return (
     <Router>
       <main className="docs">
